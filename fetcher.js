98fb7c91069456cce647a1b725f5ea4eaec12bbb
Amohamed96 <amohamed.rg@gmail.com>
2021-11-04 14:01:14 -0400

    Eslint

--- fetcher.js
+++ fetcher.js
@@ -1,31 +1,31 @@
 const request = require('request');
-const fs = require('fs')
+const fs = require('fs');
 
-let url = process.argv[2]
-let filePath = process.argv[3]
+let url = process.argv[2];
+let filePath = process.argv[3];
 
 
-const getData = function (url, filePath) {
+const getData = function(url, filePath) {
   request(url, (error, response, body) => {
-  if (response) {
-    console.log("Data retrieved");
-    printData(filePath, body);
-  } else {
-    console.log(`No website found. ${error}`)
-  }
-});
-}
+    if (response) {
+      console.log("Data retrieved");
+      printData(filePath, body);
+    } else {
+      console.log(`No website found. ${error}`);
+    }
+  });
+};
 
 const printData = function(filePath, body) {
   fs.writeFile(filePath, body, err => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      return;
     } else {
       console.log("New file created!");
-      const length = (new TextEncoder().encode(body)).length
+      const length = (new TextEncoder().encode(body)).length;
       console.log(`${length} bytes`);
     }
-  })
-}
+  });
+};
 getData(url, filePath);
